88cd1380485c6dc80f015662f66d457e15cadee0
fediaNeyli <fedianeyli@yahoo.fr>
2017-03-13 16:00:15 +0100

    a

--- src/test/java/qualiteTest/tableau/AppTest.java
+++ src/test/java/qualiteTest/tableau/AppTest.java
@@ -11,7 +11,7 @@ public class AppTest
     extends TestCase
 {
     /**
-     * Create ssthe test case
+     * Create ssfffthe test case
      *
      * @param testName name of the test case
      */
